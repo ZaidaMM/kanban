c888ec0dbe9d74fb72c83686c29e2542d2dc6d8c
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-10-30 13:59:58 +0000

    mode toggler setup

--- src/components/ModeToggler.tsx
+++ src/components/ModeToggler.tsx
@@ -1,4 +1,17 @@
+import { Dark, Light } from './Icons';
+import ToggleSwitch from './ToggleSwitch';
+
 const ModeToggler = () => {
-  return <div>ModeToggler</div>;
+  return (
+    <div className='mode-toggler-container'>
+      <span className='mode-icon'>
+        <Light />
+      </span>
+      <ToggleSwitch />
+      <span className='mode-icon'>
+        <Dark />
+      </span>
+    </div>
+  );
 };
 export default ModeToggler;
