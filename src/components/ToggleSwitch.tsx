4e3b570936ecb95f1709c67f7e1d43c0fed01a16
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-10-30 15:03:32 +0000

    se

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -1,8 +1,17 @@
+import { useThemeContext } from '../provider/ThemeModeProvider';
+
 const ToggleSwitch = () => {
+  const { handleToggleTheme } = useThemeContext();
   return (
     <div className='switch-toggle'>
       <div className='button-check' id='button-check'>
-        <input type='checkbox' className='checkbox' />
+        <input
+          type='checkbox'
+          className='checkbox'
+          name='toggleSwitch'
+          id='toggleSwitch'
+          onChange={(e) => handleToggleTheme()}
+        />
         <span className='switch-btn'></span>
         <span className='layer'></span>
         <span className='layer'></span>
